6af88251bc55e3c0348287705ecd93af62bc50ef
likun <kun.li@weimob.com>
2018-08-03 17:39:27 +0800

    添加集合

--- src/main/java/year2018/DemoEnum.java
+++ src/main/java/year2018/DemoEnum.java
@@ -1,5 +1,9 @@
 package year2018;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author likun
  * @version V1.0
@@ -7,22 +11,22 @@ package year2018;
  * @date 2018/1/15 13:39
  */
 public enum DemoEnum {
-    ONE_DAT("001","第一天"),
-    TWO_DAT("002","第二天");
+    ONE_DAT(1,"第一天"),
+    TWO_DAT(2,"第二天");
 
-    private String code;
+    private int code;
     private String name;
 
-    DemoEnum(String code, String name) {
+    DemoEnum(int code, String name) {
         this.code = code;
         this.name = name;
     }
 
-    public String getCode() {
+    public int getCode() {
         return code;
     }
 
-    public void setCode(String code) {
+    public void setCode(int code) {
         this.code = code;
     }
 
@@ -32,5 +36,16 @@ public enum DemoEnum {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public static List<Integer> getCatalogLevelList() {
+        List<Integer> list = new ArrayList<>();
+        list.add(ONE_DAT.code);
+        list.add(TWO_DAT.getCode());
+        return list;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(getCatalogLevelList().toArray()) );
     }
 }
